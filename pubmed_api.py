0599e929d028ab0eaaa62f50e8480f3e26b52016
Caio Pumar <caiopumar@gmail.com>
2020-02-19 17:30:07 +0900

    Update pubmed_api.py

--- pubmed_api.py
+++ pubmed_api.py
@@ -2,8 +2,8 @@
 
 import requests
 from bs4 import BeautifulSoup as bs
-
-
+import os
+import time
 
 
 class PubMedScraper(object):
@@ -17,15 +17,24 @@ class PubMedScraper(object):
         self.db = 'pubmed'
         self.retmode = 'xml'
     
+    def __query_builder(self,
+                      input_keywords):
+        return input_keywords.replace(' ','+')
+    
     def search(self,
                query,
                max_results=15000):
+        
+        if ' ' in query:
+            query = self.__query_builder(query)
         
         search_url = self.base + 'esearch.fcgi?db=' + self.db + '&retmode=' + \
         self.retmode + '&retmax=' + str(max_results) + '&term=' + query + \
         '&sort=relevance'+'&usehistory=y'
 
         search_response = requests.get(search_url,proxies = self.proxies)
+        
+        time.sleep(5)
         
         soup = bs(search_response.text, 'xml')
         
@@ -41,10 +50,10 @@ class PubMedScraper(object):
 
                 if (retmin + 10000) > article_count:
                     retmax = article_count
-                    print('Making an API request for articles numbered ' + str(retmin)+ 'to ' + str(retmax))
+                    print('Making an API request for articles numbered ' + str(retmin)+ ' to ' + str(retmax))
                 else:
                     retmax = retmin + 9999
-                    print('Making an API request for articles numbered ' + str(retmin)+ 'to ' + str(retmax))
+                    print('Making an API request for articles numbered ' + str(retmin)+ ' to ' + str(retmax))
                     
                 fetch_url = self.base + 'efetch.fcgi?db=' + self.db + \
                 '&query_key=' + qkey + '&WebEnv=' + webenv +'&retmin=' + \
@@ -52,6 +61,8 @@ class PubMedScraper(object):
                 str(retmax)+'&rettype=xml&retmode=xml'
                 
                 fetch_response = requests.get(fetch_url, proxies = self.proxies)
+                
+                time.sleep(5)
                 
                 self.results.append(bs(fetch_response.text, 'xml'))
                 
@@ -67,9 +78,7 @@ class PubMedScraper(object):
             fetch_soup = bs(fetch_response.text, 'xml')
             
             self.results = [fetch_soup]
-        
-        
-    
+
     
     def __content_extract(self,
                        article):
@@ -82,7 +91,13 @@ class PubMedScraper(object):
             article.find('ArticleDate').find('Month').text + '-' +\
             article.find('ArticleDate').find('Day').text)
         except:
-            contents.append('')
+            try:
+                contents.append(article.find('History').find('Year').text + '-' +\
+                article.find('History').find('Month').text + '-' +\
+                article.find('History').find('Day').text)
+            except:
+                contents.append('')
+        
         author_list = article.find_all('Author')
         if len(author_list) == 0:
             contents.append('')
@@ -146,21 +161,25 @@ class PubMedScraper(object):
         else:
             contents.append('')
         
+        contents.append(article.find('Journal').find('Title').text)
+        
         contents.append(' '.join([abst.text for abst in article.find_all('AbstractText')]).replace('\n',''))
         
         contents = ['"' + content.replace('"','').replace('\n','').strip() + '"' for content in contents]
         return contents
             
-            
+       
+    
     def saveas(self,
                file = '',
                filename=''):
         
-            
-        if file == 'xml':   
-            with open(filename,'w') as f:
-                f.write("\n".join(self.results))
-                    
+        ### Still broken, needs fixing
+        if file == 'xml':
+            for result in self.results:
+                with open(filename,'w') as f:
+                    f.write("\n".join(str(result,encoding='utf-8') ))
+        ###
         if file == 'csv':
             article_line = list()
             
@@ -169,13 +188,13 @@ class PubMedScraper(object):
                     article_line.append(self.__content_extract(article))
                     
             with open(filename, 'w',encoding='utf-8') as f:
-                f.write('"Article Title","Published Date","Main Author","Main Author Affiliation","Secondary Author(s)","Secondary Author(s) Affiliation","Keywords","Abstract"\n')
+                f.write('"Article Title","Published Date","Main Author","Main Author Affiliation","Secondary Author(s)","Secondary Author(s) Affiliation","Keywords","Journal Name","Abstract"\n')
                 for row in article_line:
                     f.write(','.join(row) + '\n')
         
 
 
-scraper = PubMedScraper()
+scraper = PubMedScraper(os.environ['http_proxy'])
 
 scraper.search(query = 'warfarin+OR+apixaban+OR+dabigatran+OR+rivaroxaban+OR+edoxaban', max_results = 15000)
 
